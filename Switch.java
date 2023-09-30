c45c1af3da3d1e377a4a8cc50cd93787dd8ae648
Akash Sharma <Sharmaaksh96145@gmail.com>
2023-09-30 23:36:24 +0530

    pattern program

--- Switch.java
+++ Switch.java
@@ -1,7 +1,7 @@
 public class Switch {
     public static void main(String[] args) {
         int num = 6;
-
+        // Enter the vlaue of num=1,2,3,4,5,6,7,8;
         switch (num) {
             case 1:
                 System.out.println("Sunday");
